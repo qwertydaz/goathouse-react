e082f77f01e537535cd529f2bbf4b2f3b65fd67b
Dáire McDermott <dairemcdermott@outlook.com>
2024-09-24 12:20:20 +0100

    removes federation and moves website to goathou.se domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import federation from '@originjs/vite-plugin-federation'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: 'goathouse',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './decision-wheel': './src/components/decision-wheel/decision-wheel.tsx',
-      },
-      shared: ['react'],
-    }),
-  ],
+  plugins: [react()],
   build: {
     modulePreload: false,
     target: 'esnext',
@@ -22,8 +11,5 @@ export default defineConfig({
   },
   server: {
     open: true,
-  },
-  preview: {
-    port: 4174,
   },
 });
